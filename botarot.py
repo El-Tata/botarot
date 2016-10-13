e74855ddd6951081bd5456af5028abeaf2d60df6
ElTata <tazio.gennuso@ensiie.fr>
2016-10-14 00:17:37 +0200

    added a sava feature

--- botarot.py
+++ botarot.py
@@ -6,6 +6,7 @@ import ircbot
 
 #import card
 import game
+import random
 
 class Botarot(ircbot.SingleServerIRCBot):
     def __init__(self):
@@ -13,6 +14,16 @@ class Botarot(ircbot.SingleServerIRCBot):
                                            "Botarot",
                                            "Bot pour jouer au tarot (indev) réalisé en Python avec ircbot")
         self.channel = "#test-ircbot"
+        self.savaTab = ["sava", "sava ", "sava ? ", "sava ! ", "sava ?! ", "sava !? ", "sava!!!! ", "sava???? ", "sava. ", "sava..."]
+        for i in range(3):
+            self.savaTab.append("sava")
+            self.savaTab.append("sava ")
+            #endFor
+        for i in range(2):
+            self.savaTab.append("sava ? ")
+            self.savaTab.append("sava ! ")
+            self.savaTab.append("sava. ")
+            #endFor
     #endDef
 
     def get_version(self):
@@ -36,10 +47,24 @@ class Botarot(ircbot.SingleServerIRCBot):
             if msg.endswith(":") or msg.endswith(" "):
                 serv.privmsg(self.channel, "J'écoute.")
             #endIf
-            if msg[9:] == "tarot":
+            if "sava" in msg:
+                self.sava(self.channel, serv)
+            elif msg[9:] == "tarot":
                 self.runGame(irclib.nm_to_n(ev.source()), serv)
             #endIf
         #endIf
+    #endDef
+
+    def sava(self, chan, serv):
+        savaStr = ""
+        rdi = int(1.0/random.expovariate(3))+1  #on simule une loi de poisson de paramètre 3, et +1 pour éviter le 0
+        if rdi > 13:
+            rdi = 13    #faut pas que ça soit, trop long, ho (et 13 c'est bien, c'est premier)
+        for i in range(rdi):
+            savaStr += self.savaTab[random.randint(0,len(self.savaTab)-1)]
+        #endFor
+        self.sendMsg(chan, savaStr)
+        return
     #endDef
 
     def runGame(self, starter, serv):
@@ -49,6 +74,7 @@ class Botarot(ircbot.SingleServerIRCBot):
 
     def sendMsg(self, dest, msg):
         self.serv.privmsg(dest, msg)
+        return
     #endDef
 
 #endClass
